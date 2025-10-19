6ee67a02b4654877636cc58d1aeca008379beba6
Sulenny <sulennyu@usf.edu>
2025-10-19 00:29:07 -0400

    retrieving product data for display

--- cp_2.js
+++ cp_2.js
@@ -14,3 +14,39 @@ function fetchProductsThen() {
    .catch(error => console.error('failed to load product data:', error));
 
 }
+
+// Use fetchProductsAsync()
+ 
+async function fetchProductsAsync() {
+    try {
+        const response = await fetch(API_URL);
+        const products = await response.json();
+        displayProducts(products);
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+// Write displayProducts(products)
+
+
+function displayProducts(products) {
+    container.innerHTML="";
+    products.slice(0,5).forEach((p) => {
+        const {name, price, image} = p.fields;
+        const card = document.createElement("div");
+        card.className = "product-card";
+        card.innerHTML = `
+        <img class= "product-image" src="${image}" alt="${name}">
+        <div class= "product-name">${name}</div>
+        <div class="product-price">$${(price/100).toFixed(2)}</div>
+        `;
+        container.appendChild(card);
+    });
+}
+
+// Reusable Error function
+
+function handleError(error) {
+    console.error(`An error Occured: ${error.message}`);
+}
